fea4df8a3033e0cb67297d48f17ca679c6985d0f
Cody Cao <codykhoa123@gmail.com>
2021-11-29 17:30:46 -0500

    VueGL setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Canvas/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue';
+import Canvas from './components/Canvas.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    Canvas
+  },   
 }
 </script>
 
---
+++ src/components/Canvas.vue
@@ -0,0 +1,122 @@
+<template>
+    <div id="app">
+        <vgl-renderer id="canvas" style="height: 60vh">
+            <template #scene>
+                <vgl-scene>
+                    <vgl-mesh antialias 
+                        :position-x="x"
+                        :position-y="y"
+                        :position-z="z"
+                    >
+                        <template #geometry>
+                            <vgl-sphere-geometry />
+                        </template>
+                        <vgl-axes-helper :size=140></vgl-axes-helper>
+                        <template #material>
+                            <vgl-mesh-standard-material />
+                        </template>
+                    </vgl-mesh>
+                    <vgl-directional-light 
+                        :position-x="1" 
+                        :position-y="1.5"   
+                        :position-z="1" 
+                    />
+                </vgl-scene>
+                </template>
+            <template #camera>
+                <vgl-perspective-camera 
+                    position="rectangular"
+                    :position-x="camX"
+                    :position-y="camY"
+                    :position-z="camZ" 
+                    rotation="lookAt"
+                />
+            </template>
+        </vgl-renderer>
+        <aside class="control-panel">
+            <h3>Object Position</h3>
+            <label> x 
+                <input 
+                    v-model.number="x"
+                    min = "-1"
+                    max = "1"
+                    step = "0.01"
+                    type = "range"
+                >
+            </label>
+            <label> y 
+                <input 
+                    v-model.number="y"
+                    min = "-1"
+                    max = "1"
+                    step = "0.01"
+                    type = "range"
+                >
+            </label>
+            <label> z
+                <input 
+                    v-model.number="z"
+                    min = "-1"
+                    max = "1"
+                    step = "0.01"
+                    type = "range"
+                >
+            </label>
+            <h3>Camera Position</h3>
+            <label> Camera's x 
+                <input 
+                    v-model.number="camX"
+                    min = "-10"
+                    max = "10"
+                    step = "0.1"
+                    type = "range"
+                >
+            </label>
+            <label> Camera's y 
+                <input 
+                    v-model.number="camY"
+                    min = "-10"
+                    max = "10"
+                    step = "0.1"
+                    type = "range"
+                >
+            </label>
+            <label> Camera's z
+                <input 
+                    v-model.number="camZ"
+                    min = "-10"
+                    max = "10"
+                    step = "0.1"
+                    type = "range"
+                >
+            </label>
+        </aside>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import * as VueGL from 'vue-gl';
+
+Object.entries(VueGL).forEach(([name, component]) => Vue.component(name, component));
+
+
+export default {
+  name: "Canvas",
+  data: ()=>({
+      x: 0,
+      y: 0,
+      z: 0,
+      upward: true,
+      camX: 0,
+      camY: 0,
+      camZ: 0,
+  }),
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
